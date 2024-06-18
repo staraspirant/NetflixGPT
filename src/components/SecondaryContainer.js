94949d1297acad4d36f41260eca9dc45193180f7
staraspirant <nehaninawe02@gmail.com>
2024-06-18 11:47:51 +0530

    feat: added movie card

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -2,10 +2,12 @@ import { useSelector } from "react-redux";
 import MovieList from "./MovieList";
 
 const SecondaryContainer = () => {
-  const movies = useSelector((store) => store.movies);
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+  //console.log(movies);
   return (
     <>
-      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+      <MovieList title={"Now Playing"} movies={movies} />
+      {/* <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} /> */}
     </>
   );
 };
